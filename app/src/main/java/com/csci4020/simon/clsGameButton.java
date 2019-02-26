3c914c791238c94aaad3407b25a9411db37c0a93
Brian Lake <bnlake@gmail.com>
2019-02-25 21:54:32 -0600

    Working to finish sound class. Still needs works

---
+++ app/src/main/java/com/csci4020/simon/clsGameButton.java
@@ -0,0 +1,6 @@
+package com.csci4020.simon;
+
+public class clsGameButton
+{
+// TODO CREATE AN OBJECT THAT REPRESENTS A BUTTON IN THE GAME.
+}
--- app/src/main/java/com/csci4020/simon/clsSoundEffect.java
+++ app/src/main/java/com/csci4020/simon/clsSoundEffect.java
@@ -5,14 +5,13 @@ import android.media.AudioAttributes;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class clsSoundEffect extends Activity
 {
+	private final int MAX_AUDIO_STREAMS = 4;
 	private SoundPool soundPool;
 	private Set<Integer> soundsLoaded;
 
@@ -22,8 +21,10 @@ public class clsSoundEffect extends Activity
 		super.onCreate(savedInstanceState);
 		soundsLoaded = new HashSet<Integer>();
 	}
+
 	@Override
-	protected void onResume() {
+	protected void onResume()
+	{
 		super.onResume();
 
 		AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
@@ -31,54 +32,43 @@ public class clsSoundEffect extends Activity
 
 		SoundPool.Builder spBuilder = new SoundPool.Builder();
 		spBuilder.setAudioAttributes(attrBuilder.build());
-		spBuilder.setMaxStreams(4);
+		spBuilder.setMaxStreams(MAX_AUDIO_STREAMS);
 		soundPool = spBuilder.build();
 
-		soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+		soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener()
+		{
 			@Override
-			public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-				if (status == 0) { // success
+			public void onLoadComplete(SoundPool soundPool, int sampleId, int status)
+			{
+				if (status == 0)
+				{
+					// success
 					soundsLoaded.add(sampleId);
-					Log.i("SOUND", "Sound loaded " + sampleId);
-				} else {
-					Log.i("SOUND", "Error cannot load sound status = " + status);
+				}
+				else
+				{
+					Log.i("4020debug", "Error cannot load sound status = " + status);
 				}
 			}
 		});
 
-		final int laughId = soundPool.load(this, R.raw.laugh, 1);
-		findViewById(R.id.laugh_button).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				playSound(laughId);
-			}
-		});
+		final int redId = soundPool.load(this, R.raw.red, 1);
+		final int greenId = soundPool.load(this, R.raw.green, 1);
+		final int blueId = soundPool.load(this, R.raw.blue, 1);
+		final int yellowId = soundPool.load(this, R.raw.yellow, 1);
 
-		final int cowId = soundPool.load(this, R.raw.cow, 1);
-		findViewById(R.id.cow_button).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				playSound(cowId);
-			}
-		});
-
-		final int clickId = soundPool.load(this, R.raw.click, 1);
-		findViewById(R.id.editText).setOnKeyListener(new View.OnKeyListener() {
-			@Override
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				Log.i("EVENT", event.getAction() + "");
-				if (event.getAction() == KeyEvent.ACTION_UP) {
-					playSound(clickId);
-				}
-				return false;
-			}
-		});
+		// TODO SET ONCLICKLISTENERS TO SOMETHING AND PLAY SOUND
 	}
 
+	/**
+	 * Need to stop any sounds effects if the user leaves the app for any reason
+	 */
 	@Override
-	protected void onPause() {
+	protected void onPause()
+	{
 		super.onPause();
-		if (soundPool != null) {
+		if (soundPool != null)
+		{
 			soundPool.release();
 			soundPool = null;
 
@@ -86,8 +76,15 @@ public class clsSoundEffect extends Activity
 		}
 	}
 
-	private void playSound(int soundId) {
-		if (soundsLoaded.contains(soundId)) {
+	/**
+	 * Plays the sound identified by the sound ID
+	 *
+	 * @param soundId int
+	 */
+	protected void playSound(int soundId)
+	{
+		if (soundsLoaded.contains(soundId))
+		{
 			soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f);
 		}
 	}
