b4520efc7b61c6a7125ed3ec8bdb133bec2c88d3
Han Kim <hanniek55@gmail.com>
2019-02-26 20:23:56 -0600

    Add layout for the game and display game when button is click in MainActivity

--- app/src/main/java/com/csci4020/simon/MainActivity.java
+++ app/src/main/java/com/csci4020/simon/MainActivity.java
@@ -1,7 +1,9 @@
 package com.csci4020.simon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), GameActivity.class));
+            }
+
+        });
+
     }
 }
