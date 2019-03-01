34b349e7a95746df6c042482e387a88172385881
Brian Lake <bnlake@gmail.com>
2019-02-28 21:49:17 -0600

    Create test layout and activity to ensure sound effect class works

--- app/src/main/java/com/csci4020/simon/MainActivity.java
+++ app/src/main/java/com/csci4020/simon/MainActivity.java
@@ -2,12 +2,48 @@ package com.csci4020.simon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.testlayout);
+
+        final clsGameSoundEffects gameSoundEffects = new clsGameSoundEffects();
+
+        findViewById(R.id.btnBlue).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                gameSoundEffects.playSound(gameSoundEffects.SOUND_EFFECT_BLUE);
+            }
+        });
+        findViewById(R.id.btnGreen).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                gameSoundEffects.playSound(gameSoundEffects.SOUND_EFFECT_GREEN);
+            }
+        });
+        findViewById(R.id.btnRed).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                gameSoundEffects.playSound(gameSoundEffects.SOUND_EFFECT_RED);
+            }
+        });
+        findViewById(R.id.btnYellow).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                gameSoundEffects.playSound(gameSoundEffects.SOUND_EFFECT_YELLOW);
+            }
+        });
     }
 }
